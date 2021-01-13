48156819506ea92593e66c18438573eaa730fb69
Yankuic Galvan <yankuic@ufl.edu>
2021-01-13 08:28:24 -0500

    Implement timeit decorator

--- omop_etl/utils.py
+++ omop_etl/utils.py
@@ -2,10 +2,11 @@
 
 import time
 from contextlib import contextmanager
+from functools import wraps
 
 @contextmanager
 def timeitc(name=''):
-    """Return elapsed time to run a code block."""
+    """Return elapsed time to run code block."""
     startTime = time.time()
 
     yield
@@ -13,3 +14,14 @@ def timeitc(name=''):
     elapsedTime = time.time() - startTime
     print('{} finished in {}'.format(name, time.strftime("%H h %M m %S s",  
                                      time.gmtime(elapsedTime))))
+
+def timeitd(f):
+    """Return elapsed time to run function."""
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        startTime = time.time()
+        result = f(*args, **kwargs)
+        elapsedTime = time.time() - startTime
+        print(f'{f.__name__} completed in {time.strftime("%H h %M m %S s", time.gmtime(elapsedTime))}')
+        return result
+    return wrap
