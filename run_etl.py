06b78a0fb6005bf5636a648d62d7b7ef54294381
Yankuic Galvan <yankuic@ufl.edu>
2021-01-13 08:33:30 -0500

    rename file

--- run_etl.py
+++ run_etl.py
@@ -3,5 +3,5 @@
 from omop_etl.postproc.load import Loader
 
 if __name__ == "__main__":
-    l = Loader('omop', 'conf.yml')
+    l = Loader(config_file='config.yml')
     print(l.load_drug_exposure())
