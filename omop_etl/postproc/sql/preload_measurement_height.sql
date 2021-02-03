4747701aa5e31b51735428989e58441bed496289
Yankuic Galvan <yankuic@ufl.edu>
2021-02-03 13:55:27 -0500

    Add join with visit_occurrence_mapping

--- omop_etl/postproc/sql/preload_measurement_height.sql
+++ omop_etl/postproc/sql/preload_measurement_height.sql
@@ -1,5 +1,6 @@
 insert into preload.measurement with (tablock)
-select person_id = b.person_id
+select distinct 
+      person_id = b.person_id
       ,measurement_concept_id = d.target_concept_id
       ,measurement_date = a.HEIGHT_DATE
       ,measurement_datetime = a.HEIGHT_DATETIME
@@ -12,7 +13,7 @@ select person_id = b.person_id
       ,range_low = NULL
       ,range_high = NULL
       ,provider_id = c.provider_id
-      ,visit_occurrence_id = NULL
+      ,visit_occurrence_id = e.visit_occurrence_id
       ,visit_detail_id = NULL
       ,measurement_source_value = d.source_code
       ,measurement_source_concept_id = d.source_concept_id
@@ -23,6 +24,8 @@ from stage.measurement_height a
 join xref.person_mapping b
 on a.patient_key = b.patient_key
 join xref.provider c 
-on c.provider_source_value = isnull(a.Attending_Provider, Visit_Provider)
+on c.provider_source_value = isnull(a.Attending_Provider, a.Visit_Provider)
 left join xref.source_to_concept_map d 
 on source_code = 'height' and source_vocabulary_id = 'Flowsheet'
+join xref.visit_occurrence_mapping e 
+on a.patnt_encntr_key = e.patnt_encntr_key
--- omop_etl/postproc/sql/preload_procedure_cpt.sql
+++ omop_etl/postproc/sql/preload_procedure_cpt.sql
@@ -1,17 +1,17 @@
 insert into preload.procedure_occurrence with (tablock)
 select distinct 
     b.[person_id]
-    ,[procedure_concept_id] = 0
+    ,[procedure_concept_id] = e.concept_id_2
     ,[procedure_date] = a.START_DATE
     ,[procedure_datetime] = a.START_DATE
     ,[procedure_type_concept_id] = 32817
     ,[modifier_concept_id] = 0
     ,[quantity] = NULL 
     ,[provider_id] = c.provider_id
-    ,[visit_occurrence_id] = 0
+    ,[visit_occurrence_id] = g.visit_occurrence_id
     ,[visit_detail_id] = 0
     ,[procedure_source_value] = a.CPT_CD
-    ,[procedure_source_concept_id] = 0
+    ,[procedure_source_concept_id] = d.concept_id
     ,[modifier_source_value] = NULL
     ,[source_table] = 'procedure_cpt'
 from [stage].[procedure_cpt] a
@@ -25,3 +25,5 @@ join xref.concept_relationship e
 on d.concept_id = e.concept_id_1 and e.relationship_id = 'Maps to'
 join xref.concept f 
 on f.concept_id = e.concept_id_2 and f.domain_id = 'Procedure'
+join xref.visit_occurrence_mapping g
+on a.patnt_encntr_key = g.patnt_encntr_key
