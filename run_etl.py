297027de5c3256c26cbea40dc737613229a18b4e
Yankuic Galvan <yankuic@ufl.edu>
2021-02-09 15:02:39 -0500

    working commit

--- run_etl.py
+++ run_etl.py
@@ -1,24 +1,25 @@
-"""."""
+"""The script that runs the ETL."""
 
-from omop_etl.postproc.load import Loader
-import os
+from omop_etl.load import Loader
 
 if __name__ == "__main__":
     l = Loader(config_file='config.yml')
 
-    print(l.update_mappings('person'))
-    print(l.update_mappings('visit_occurrence'))
+    # print(l.update_mappings('person'))
+    # print(l.update_mappings('visit_occurrence'))
     
-    print(l.preload_all())
+    print(l.preload('condition_occurrence'))
+    # print(l.preload_all())
     
-    print(l.load_table('person'))
-    print(l.load_table('death'))
-    print(l.load_table('condition_occurrence'))
-    print(l.load_table('procedure_occurrence'))
-    print(l.load_table('drug_exposure'))
-    print(l.load_table('observation'))
-    print(l.load_table('provider'))
-    print(l.load_table('care_site'))
-    print(l.load_table('location'))
+    # print(l.load_table('person'))
+    # print(l.load_table('death'))
+    # print(l.load_table('condition_occurrence'))
+    # print(l.load_table('procedure_occurrence'))
+    # print(l.load_table('drug_exposure'))
+    # print(l.load_table('observation'))
+    # print(l.load_table('provider'))
+    # print(l.load_table('care_site'))
+    # print(l.load_table('location'))
 
-    # print(os.path.dirname(os.path.abspath(__file__)))
+    # print(inspect.getfullargspec(l.load_table))
+    
