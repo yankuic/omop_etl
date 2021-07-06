87c5fa4f4fc4d8df10836ecd673dd0315763b246
Yankuic Galvan <yankuic@ufl.edu>
2021-07-06 08:58:07 -0400

    Fix tidal volume exhaled

--- omop_etl/sql/preload_measurement_res_tidal_mech.sql
+++ omop_etl/sql/preload_measurement_res_tidal_mech.sql
@@ -18,14 +18,14 @@ select person_id = b.person_id
     ,measurement_source_concept_id = isnull(d.source_concept_id,0)
     ,unit_source_value = 'mL'
     ,value_source_value = a.tidal_volume_exhaled
-    ,source_table = 'measurement_res_tidal_exhaled'
-from stage.MEASUREMENT_Res_TidalExhaled a
+    ,source_table = 'measurement_res_tidal_mech'
+from stage.MEASUREMENT_Res_Tidal_Mech a
 join xref.person_mapping b
 on a.patient_key = b.patient_key
 left join xref.provider_mapping c 
 on c.providr_key = isnull(a.attending_provider, a.visit_provider)
 left join xref.source_to_concept_map d 
-on source_code = 'TIDAL VOLUME EXHALED' and source_vocabulary_id = 'Flowsheet'
+on source_code = 'TIDAL VOLUME - Mechanical Ventilator' and source_vocabulary_id = 'Flowsheet'
 left join xref.visit_occurrence_mapping e 
 on a.patnt_encntr_key = e.patnt_encntr_key
 where b.active_ind = 'Y'
