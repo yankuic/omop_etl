ea440a7798928f1478e79d62cbd292bf46dc5b31
Yankuic Galvan <yankuic@ufl.edu>
2021-08-30 15:36:59 -0400

    Implement indexing

--- omop_etl/sql/load_measurement.sql
+++ omop_etl/sql/load_measurement.sql
@@ -1,3 +1,10 @@
+drop index if exists
+      [ix_measurementconceptid_personid] ON [dbo].[measurement],
+      [ix_personid_measurementconceptid] ON [dbo].[measurement]
+
+alter table [dbo].[measurement] 
+drop constraint if exists [pk_measurement]
+
 insert into dbo.measurement with (tablock) (
      person_id
     ,measurement_concept_id
@@ -39,3 +46,49 @@ select distinct person_id
     ,unit_source_value
     ,value_source_value
 from preload.measurement
+
+create nonclustered index [ix_measurementconceptid_personid] 
+on [dbo].[measurement]
+(
+	[measurement_concept_id] ASC,
+	[person_id] ASC
+) with (PAD_INDEX = OFF
+       ,STATISTICS_NORECOMPUTE = OFF
+       ,SORT_IN_TEMPDB = OFF
+       ,DROP_EXISTING = OFF
+       ,ONLINE = OFF
+       ,ALLOW_ROW_LOCKS = ON
+       ,ALLOW_PAGE_LOCKS = ON
+       ,OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF
+) ON [fg_user1]
+
+create nonclustered index [ix_personid_measurementconceptid] 
+on [dbo].[measurement]
+(
+	[person_id] ASC,
+	[measurement_concept_id] ASC
+)with (PAD_INDEX = OFF
+      ,STATISTICS_NORECOMPUTE = OFF
+      ,SORT_IN_TEMPDB = OFF
+      ,DROP_EXISTING = OFF
+      ,ONLINE = OFF
+      ,ALLOW_ROW_LOCKS = ON
+      ,ALLOW_PAGE_LOCKS = ON
+      ,OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF
+) ON [fg_user1]
+
+alter table [dbo].[measurement] 
+add constraint [pk_measurement] PRIMARY KEY CLUSTERED 
+(
+	[measurement_id] ASC,
+	[person_id] ASC,
+	[measurement_concept_id] ASC
+)with (PAD_INDEX = OFF
+      ,STATISTICS_NORECOMPUTE = OFF
+      ,SORT_IN_TEMPDB = OFF
+      ,IGNORE_DUP_KEY = OFF
+      ,ONLINE = OFF
+      ,ALLOW_ROW_LOCKS = ON
+      ,ALLOW_PAGE_LOCKS = ON
+      ,OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF
+) ON [fg_user1]
