e30a27fd9f8da815e3efb6b4690236db2c344e4f
Yankuic Galvan <yankuic@ufl.edu>
2021-11-04 11:02:18 -0400

    Prevent loading patients and visits not in person/visit_occurrence

--- omop_etl/sql/load_procedure.sql
+++ omop_etl/sql/load_procedure.sql
@@ -20,18 +20,24 @@ insert into dbo.procedure_occurrence with (tablock) (
     ,[procedure_source_concept_id]
     ,[modifier_source_value]
 )
-select person_id
+select a.person_id
     ,[procedure_concept_id]
     ,[procedure_date]
     ,[procedure_datetime]
     ,[procedure_type_concept_id]
     ,[modifier_concept_id]
     ,[quantity]
-    ,[provider_id]
+    ,a.provider_id
     ,[visit_occurrence_id]
     ,[visit_detail_id]
     ,[procedure_source_value]
     ,[procedure_source_concept_id]
     ,[modifier_source_value]
-from preload.procedure_occurrence
-
+from preload.procedure_occurrence a
+join dbo.person b
+on a.person_id = b.person_id
+where visit_occurrence_id is null 
+or visit_occurrence_id in (
+      select visit_occurrence_id 
+      from dbo.visit_occurrence
+)
