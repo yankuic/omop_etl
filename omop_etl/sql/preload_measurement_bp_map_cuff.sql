1a2413efb13428de296a0af65568b5eb54b2a9db
tmagoc <tmagoc@ufl.edu>
2022-01-13 16:17:59 -0500

    Upload New File

---
+++ omop_etl/sql/preload_measurement_bp_map_cuff.sql
@@ -0,0 +1,47 @@
+drop table if exists #measurement_bp
+;with bp as (
+	select patient_key
+            ,patnt_encntr_key
+            ,bp_date
+            ,bp_datetime
+            ,bp_measure = 'map - cuff'
+            ,bp_value = map_cuff
+            ,bp_raw_value = map_cuff
+            ,[provider] = isnull(attending_provider, visit_provider)
+      from stage.MEASUREMENT_BP_MAP_Cuff
+      where map_cuff is not null      
+)
+
+insert into preload.measurement with (tablock)
+select person_id = b.person_id
+      ,measurement_concept_id = isnull(d.target_concept_id, 0)
+      ,measurement_date = a.BP_DATE
+      ,measurement_datetime = a.BP_DATETIME
+      ,measurement_time = CAST(a.BP_DATETIME as TIME)
+      ,measurement_type_concept_id = 32817
+      ,operator_concept_id = NULL
+      ,value_as_number = a.bp_value
+      ,value_as_concept_id = NULL
+      ,unit_concept_id = NULL
+      ,range_low = NULL
+      ,range_high = NULL
+      ,provider_id = c.provider_id
+      ,visit_occurrence_id = e.visit_occurrence_id
+      ,visit_detail_id = NULL
+      ,measurement_source_value = isnull(d.source_code, a.bp_measure)
+      ,measurement_source_concept_id = isnull(d.source_concept_id, 0)
+      ,unit_source_value = NULL
+      ,value_source_value = a.bp_raw_value
+      ,source_table = 'measurement_bp_map_cuff'
+from bp a 
+join xref.person_mapping b
+on a.patient_key = b.patient_key
+left join xref.provider_mapping c 
+on c.providr_key = a.provider
+left join xref.source_to_concept_map d 
+on d.source_code = a.bp_measure
+left join xref.visit_occurrence_mapping e 
+on a.patnt_encntr_key = e.patnt_encntr_key
+where b.active_ind = 'Y'
+ 
+drop table if exists #measurement_bp
