303a966c15068d5a87cdbd3ebdb3137fbe6baf1c
tmagoc <tmagoc@ufl.edu>
2022-01-13 16:25:39 -0500

    Upload New File

---
+++ omop_etl/sql/preload_measurement_res_vent_start_peds.sql
@@ -0,0 +1,31 @@
+insert into preload.measurement with (tablock)
+select person_id = b.person_id
+    ,measurement_concept_id = isnull(d.target_concept_id,0)
+    ,measurement_date = a.Respiratory_Date
+    ,measurement_datetime = a.Respiratory_Datetime
+    ,measurement_time = cast(a.Respiratory_Datetime as TIME)
+    ,measurement_type_concept_id = 32817
+    ,operator_concept_id = NULL
+    ,value_as_number = NULL 
+    ,value_as_concept_id = NULL
+    ,unit_concept_id = NULL
+    ,range_low = NULL
+    ,range_high = NULL
+    ,provider_id = c.provider_id
+    ,visit_occurrence_id = e.visit_occurrence_id
+    ,visit_detail_id = NULL
+    ,measurement_source_value = d.source_code
+    ,measurement_source_concept_id = isnull(d.source_concept_id, 0)
+    ,unit_source_value = NULL
+    ,value_source_value = a.respiratory_vent_bgn
+    ,source_table = 'measurement_res_vent_start_peds'
+from stage.MEASUREMENT_Res_Vent_Start_Peds a  
+join xref.person_mapping b
+on a.patient_key = b.patient_key
+left join xref.provider_mapping c 
+on c.providr_key = isnull(a.Attending_Provider, a.Visit_Provider)
+left join xref.source_to_concept_map d 
+on source_code = 'VENT START - Peds' and source_vocabulary_id = 'Flowsheet'
+left join xref.visit_occurrence_mapping e 
+on a.patnt_encntr_key = e.patnt_encntr_key
+where b.active_ind = 'Y'
