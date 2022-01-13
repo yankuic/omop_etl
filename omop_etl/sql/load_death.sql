8966c82b330f219921a3f267bcfbb3291463af39
tmagoc <tmagoc@ufl.edu>
2022-01-13 16:12:13 -0500

    Upload New File

---
+++ omop_etl/sql/load_death.sql
@@ -0,0 +1,13 @@
+
+insert into dbo.death
+select b.[person_id]
+      ,[death_date] = ISNULL(a.PATNT_DTH_DATE, a.PATNT_SSN_DTH_DATE)  --Select Epic death date if exists; otherwise select Social Security Death index death date.
+      ,[death_datetime] = ISNULL(a.PATNT_DTH_DATE, a.PATNT_SSN_DTH_DATE)
+      ,[death_type_concept_id] = CASE WHEN a.PATNT_DTH_DATE IS NULL THEN 32885 ELSE 32817 END
+      ,[cause_concept_id] = 0
+      ,[cause_source_value] = NULL
+      ,[cause_source_concept_id] = 0
+  from stage.death a
+  join xref.person_mapping b
+  on a.PATIENT_KEY = b.patient_key
+where b.active_ind = 'Y'
