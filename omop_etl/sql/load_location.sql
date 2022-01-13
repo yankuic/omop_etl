d573cedd5388d5f67840994a34588a6df454d379
tmagoc <tmagoc@ufl.edu>
2022-01-13 16:12:49 -0500

    Upload New File

---
+++ omop_etl/sql/load_location.sql
@@ -0,0 +1,22 @@
+insert into dbo.location with (tablock) 
+select b.location_id
+    --Use explicit truncation to avoid truncation errors
+    ,[address_1] = left(addr1, 50)
+    ,[address_2] = left(addr2, 50)
+    ,[city_name]
+    ,[state] = left(state_abbrv, 2)
+    ,[zip3_cd]
+    ,[county] = left(cnty_name, 20)
+    ,[location_source_value] = left(a.[addr_key], 50)
+from stage.location a 
+join xref.location_mapping b 
+on a.addr_key = b.addr_key 
+where b.location_id in (
+    select distinct location_id
+    from (
+        select location_id from person
+        union 
+        select location_id from care_site
+    ) b
+) 
+and b.active_ind = 'Y'
