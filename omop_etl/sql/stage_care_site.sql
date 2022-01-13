d3adddd0c1caed0269e93964fe6ca0d6139a15a6
tmagoc <tmagoc@ufl.edu>
2022-01-13 16:30:06 -0500

    Upload New File

---
+++ omop_etl/sql/stage_care_site.sql
@@ -0,0 +1,12 @@
+drop table if exists stage.CARE_SITE
+select distinct
+	ADDR_KEY
+	,RPT_LVL4_DESC
+	,CURR_LOCATN_NAME
+	,LOCATN_KEY
+	,DEPT_ID 
+	,DEPT_NAME
+into stage.CARE_SITE
+from dws_prod.dbo.ALL_HOSPITAL_ORGANIZATIONS a
+where DEPT_ID is not null
+and DEPT_ID <> '0'
