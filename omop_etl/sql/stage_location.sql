0c86c1b06cc3ed0a329d4bd957a846b05cf14a08
tmagoc <tmagoc@ufl.edu>
2022-01-13 16:30:16 -0500

    Upload New File

---
+++ omop_etl/sql/stage_location.sql
@@ -0,0 +1,15 @@
+drop table if exists stage.LOCATION
+select distinct 
+    ADDR_KEY
+    ,ADDR1
+    ,ADDR2
+    ,a.CITY_NAME
+    ,a.STATE_ABBRV
+    ,ZIP3_CD
+    ,b.CNTY_NAME
+    ,a.LATITUDE
+    ,a.LONGITUDE
+into stage.LOCATION
+from dws_prod.dbo.ALL_ADDRESSES a
+left join dws_prod.dbo.ZIP_CODE_MAPPER_XREF b 
+on a.ZIP5_CD = b.ZIP_CD
